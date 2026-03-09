a2d1abe58ec695f0d628b630b07ed8c0b0e7f610
Ahrar Shah <ahrar.09321@gmail.com>
2026-03-09 23:06:11 +0500

    Server Issue Fixed!

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ app.get("/", (req, res) => {
   res.send("Hi From Server | MongoDB Successfully Connected!");
 });
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
-})
+// app.listen(5000, () => {
+//   console.log("Server is running on port 5000");
+// })
 
-// export default app;
+export default app;
